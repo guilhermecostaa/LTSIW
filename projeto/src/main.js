e636b0c419489671cf056d74ce69e67dcafd015d
Guilherme  Costa <guilhermecosta@AirdeGuilherme.lan>
2020-12-24 16:08:51 +0000

    bootstrap and sweet alert

--- projeto/src/main.js
+++ projeto/src/main.js
@@ -2,6 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import BootstrapVue from 'bootstrap-vue'
+import VueSweetalert2 from 'vue-sweetalert2';
+
+import 'sweetalert2/dist/sweetalert2.min.css';
+
+Vue.use(BootstrapVue);
+Vue.use(VueSweetalert2);
 
 Vue.config.productionTip = false;
 
