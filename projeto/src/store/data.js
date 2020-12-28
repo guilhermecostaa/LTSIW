0e671be652454db5ed08e38d8e7abb3c1471041f
Guilherme  Costa <guilhermecosta@AirdeGuilherme.lan>
2020-12-28 18:17:37 +0000

    users

---
+++ projeto/src/store/data.js
@@ -0,0 +1,7 @@
+module.exports = {
+    users: [
+        { id: 1, name: "Guilherme", number: "9170193", password: "1234", userType: "admin" },
+        { id: 2, name: "Nuno", number: "9170197", password: "1234", userType: "student" },
+        { id: 3, name: "Gustavo", number: "9170196", password: "1234", userType: "teacher" }
+    ]
+}
