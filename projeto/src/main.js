80e3c44b25c996650d27e2dacff021700b3222d0
Guilherme  Costa <guilhermecosta@AirdeGuilherme.lan>
2020-12-28 18:17:29 +0000

    some changes

--- projeto/src/main.js
+++ projeto/src/main.js
@@ -6,9 +6,13 @@ import BootstrapVue from 'bootstrap-vue'
 import VueSweetalert2 from 'vue-sweetalert2';
 
 import 'sweetalert2/dist/sweetalert2.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.use(BootstrapVue);
 Vue.use(VueSweetalert2);
+
+
 
 Vue.config.productionTip = false;
 
--- projeto/src/router/index.js
+++ projeto/src/router/index.js
@@ -1,66 +1,65 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Login from '../views/Login.vue'
-import Students from '../views/Students.vue'
-import SearchStudents from '../views/SearchStudents.vue'
-import Backoffice from '../views/Backoffice/Backoffice.vue'
-import BackofficeUsers from '../views/Backoffice/BackofficeUsers.vue'
-import BackofficeAtividades from '../views/Backoffice/BackofficeAtividades.vue'
-import BackofficeUcs from '../views/Backoffice/BackofficeUcs.vue'
-import BackofficeFormula from '../views/Backoffice/BackofficeFormula.vue'
-import Rankings from '../views/Rankings.vue'
+import Login from "../views/Login.vue";
+import Students from "../views/Students.vue";
+import SearchStudents from "../views/SearchStudents.vue";
+import Backoffice from "../views/Backoffice/Backoffice.vue";
+import BackofficeUsers from "../views/Backoffice/BackofficeUsers.vue";
+import BackofficeAtividades from "../views/Backoffice/BackofficeAtividades.vue";
+import BackofficeUcs from "../views/Backoffice/BackofficeUcs.vue";
+import BackofficeFormula from "../views/Backoffice/BackofficeFormula.vue";
+import Rankings from "../views/Rankings.vue";
 
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'login',
-    component: Login,
+    path: "/",
+    name: "login",
+    component: Login
   },
   {
-    path: '/alunos',
-    name: 'searchStudents',
-    component: SearchStudents,
+    path: "/searchStudents",
+    name: "searchStudents",
+    component: SearchStudents
   },
   {
-    path: '/alunos/:id',
-    name: 'students',
-    component: Students,
+    path: "/students/:id",
+    name: "students",
+    component: Students
   },
   {
-    path: '/rankings',
-    name: 'rankings',
-    component: Rankings,
+    path: "/rankings",
+    name: "rankings",
+    component: Rankings
   },
   {
-    path: '/backoffice',
-    name: 'backoffice',
+    path: "/backoffice",
     component: Backoffice,
     children: [
       {
-        path: '/users',
-        name: 'backofficeUsers',
+        path: "users",
+        name: "backofficeUsers",
         component: BackofficeUsers
       },
       {
-        path: '/atividades',
-        name: 'backofficeAtividades',
+        path: "atividades",
+        name: "backofficeAtividades",
         component: BackofficeAtividades
       },
       {
-        path: '/ucs',
-        name: 'backofficeUcs',
+        path: "ucs",
+        name: "backofficeUcs",
         component: BackofficeUcs
       },
       {
-        path: '/formula',
-        name: 'backofficeFormula',
+        path: "formula",
+        name: "backofficeFormula",
         component: BackofficeFormula
-      },
-    ],
-  },
+      }
+    ]
+  }
 ];
 
 const router = new VueRouter({
