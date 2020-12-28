d409342deee200df9bda8ce869b8dec8af33e860
Guilherme  Costa <guilhermecosta@AirdeGuilherme.lan>
2020-12-28 18:17:48 +0000

    some changes

--- projeto/src/store/index.js
+++ projeto/src/store/index.js
@@ -4,8 +4,52 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
-});
+  state: {
+    users: [],
+    loggedUserId: 0
+  },
+  mutations: {
+    USER_LOGGED_IN(state, id) {
+      state.loggedUserId = id;
+    },
+    USER_LOGGED_OUT(state) {
+      state.loggedUserId = 0;
+    },
+    SET_USERS(state, users) {
+      state.users = users;
+    }
+  },
+  getters: {
+    getUserByNumber: state => number => {
+      return state.users.find(user => user.number === number);
+    },
+    getUsers: state => {
+      return state.users;
+    },
+    getLoggedUserId: state => {
+      return state.loggedUserId;
+    },
+    getUserById: state => id => {
+      return state.users.find(user => user.id === id);
+    },
+    getLoggedUsernameById: state => id => {
+      let name = "";
+      for (let i = 0; i < state.users.length; i++) {
+        if (id === state.users.id) {
+          name = state.users[i].name;
+        }
+      }
+      return name;
+    },
+    getUserTypeById: state => id => {
+      let type = "";
+      for (let i = 0; i < state.users.length; i++) {
+        if (id === state.users[i].id) {
+          type = state.users[i].userType;
+        }
+      }
+      
+      return type;
+    }
+  }
+})
