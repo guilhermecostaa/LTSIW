e3aa02243d9e68692d8c3086d62c8aea8a099096
Guilherme  Costa <guilhermecosta@AirdeGuilherme.lan>
2020-12-28 18:18:55 +0000

    Students not done yet

---
+++ projeto/src/views/Students.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="addUser">
+    <b-form @submit.prevent="addUser">
+      <b-form-group id="input-name" label="Nome" label-for="input-name">
+        <b-form-input id="input-name" v-model="form.name" type="text" required></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-number" label="Número" label-for="input-number">
+        <b-form-input id="input-number" v-model="form.number" type="text" required></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-password" label="Password" label-for="input-password">
+        <b-form-input id="input-password" v-model="form.password" type="password" required></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-userType" label="User Type" label-for="input-userType">
+        <div class="form-group">
+          <label for></label>
+          <select class="form-control" name id="input-userType" v-model="form.userType">
+            <option>Admin</option>
+            <option>Student</option>
+            <option>Teacher</option>
+          </select>
+        </div>
+      </b-form-group>
+
+      <b-button class="col-6" type="submit" variant="primary">Submit</b-button>
+    </b-form>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import { mapGetters } from "vuex";
+export default {
+  name: "Students",
+  data() {
+    return {
+      
+    };
+  },
+  methods: {
+    
+  },
+  computed: {
+    ...mapGetters(["getUserByNumber"])
+  }
+};
+</script>
