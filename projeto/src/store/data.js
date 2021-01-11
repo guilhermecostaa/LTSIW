9a4805df7eb023283a71740e03fe0cf027a13edc
Guilherme  Costa <guilhermecosta@AirdeGuilherme.lan>
2021-01-11 17:54:44 +0000

    router changes

--- projeto/src/store/data.js
+++ projeto/src/store/data.js
@@ -5,13 +5,9 @@ module.exports = {
         { id: 3, name: "Gustavo", number: "9170196", password: "1234", userType: "teacher" }
     ],
     ucs: [
-        { id: 1, name: "Programação Web I", abbreviation: "PWI" },
-        { id: 2, name: "Base de Dados", abbreviation: "BD" },
-        { id: 3, name: "Sistemas Gráficos", abbreviation: "SG" },
-        { id: 4, name: "Serviços Interface para a Cloud", abbreviation: "SIC" },
-        { id: 5, name: "Prototipagem Avançada em Plataformas Digitais", abbreviation: "PAPD" },
-        { id: 6, name: "Inovação e Empreendorismo", abbreviation: "IE" },
-        { id: 7, name: "Computação Móvel e Ubíqua", abbreviation: "CMU" }
+        { id: 1, name: "Programação Web I", abbreviation: "PWI", teacher: 3},
+        { id: 2, name: "Computação Móvel e Ubíqua", abbreviation: "CMU", teacher: 3},
+        { id: 3, name: "Programação Web II", abbreviation: "PWII", teacher: 3},
     ],
     activities: [
         {
@@ -24,19 +20,7 @@ module.exports = {
             hourEnd: "11:00",
             dateStart: "2018-11-20",
             durationDays: 1,
-            dateEnd: "2018-11-20",
-            picture: {
-                thumbnail: "https://i.imgur.com/9kTBtgg.jpg",
-                poster: {
-                    url: "https://i.imgur.com/MXuI7WL.jpg",
-                    orientation: "Vertical"
-                },
-                gallery: [
-                    "http://eventos.esmad.ipp.pt/plug-in/img/galeria/gal_10.jpg",
-                    "http://eventos.esmad.ipp.pt/plug-in/img/galeria/gal_07.jpg",
-                    "http://eventos.esmad.ipp.pt/plug-in/img/galeria/gal_08.jpg"
-                ]
-            }
+            dateEnd: "2018-11-20"
         },
         {
             id: 2,
@@ -48,19 +32,7 @@ module.exports = {
             hourEnd: "11:00",
             dateStart: "2018-11-20",
             durationDays: 1,
-            dateEnd: "2018-11-20",
-            picture: {
-                thumbnail: "https://i.imgur.com/9kTBtgg.jpg",
-                poster: {
-                    url: "https://i.imgur.com/MXuI7WL.jpg",
-                    orientation: "Vertical"
-                },
-                gallery: [
-                    "http://eventos.esmad.ipp.pt/plug-in/img/galeria/gal_10.jpg",
-                    "http://eventos.esmad.ipp.pt/plug-in/img/galeria/gal_07.jpg",
-                    "http://eventos.esmad.ipp.pt/plug-in/img/galeria/gal_08.jpg"
-                ]
-            }
+            dateEnd: "2018-11-20"
         },
         {
             id: 3,
@@ -72,19 +44,7 @@ module.exports = {
             hourEnd: "11:00",
             dateStart: "2018-11-20",
             durationDays: 1,
-            dateEnd: "2018-11-20",
-            picture: {
-                thumbnail: "https://i.imgur.com/9kTBtgg.jpg",
-                poster: {
-                    url: "https://i.imgur.com/MXuI7WL.jpg",
-                    orientation: "Vertical"
-                },
-                gallery: [
-                    "http://eventos.esmad.ipp.pt/plug-in/img/galeria/gal_10.jpg",
-                    "http://eventos.esmad.ipp.pt/plug-in/img/galeria/gal_07.jpg",
-                    "http://eventos.esmad.ipp.pt/plug-in/img/galeria/gal_08.jpg"
-                ]
-            }
+            dateEnd: "2018-11-20"
         }
     ]
 }
