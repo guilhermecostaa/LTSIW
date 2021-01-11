67152d49d7bb332c87ea08ec60d214c9d33536f4
Guilherme  Costa <guilhermecosta@AirdeGuilherme.lan>
2021-01-11 17:54:04 +0000

    backoffice ucs done

--- projeto/src/views/Backoffice/BackofficeUcs.vue
+++ projeto/src/views/Backoffice/BackofficeUcs.vue
@@ -1,39 +1,85 @@
 <template>
-  <div id="addUc  container">
-    <b-form @submit.prevent="addUcs">
+  <div id="addUc container">
+    <b-form @submit.prevent="addUc">
       <b-form-group id="input-name" label="Nome" label-for="input-name">
         <b-form-input id="input-name" v-model="form.name" type="text" required></b-form-input>
       </b-form-group>
 
       <b-form-group id="input-teacher" label="Professor" label-for="input-teacher">
         <div class="form-group">
-          <b-form-select v-model="form.teacher" :options="getTeachersByUserType('teacher')"></b-form-select>
+          <b-form-select v-model="form.teacher" :options="getUsersByUserType('teacher').map(teacher => {return {value: teacher.id, text: teacher.name}})" required></b-form-select>
         </div>
       </b-form-group>
 
-      <b-button class="col-6" type="submit" variant="primary">Submit</b-button>
+      <b-button class="col-6" type="submit" variant="primary">Adicionar</b-button>
     </b-form>
+
+    <br />
+    <br />
+
+    <h1>Ucs</h1>
+    <div class="dataTable container">
+      <DataTable :items="getUcs.map(uc => {return {name: uc.name, teacher: getUserById(uc.teacher).name, abbreviation: uc.abbreviation}})" :fields="['name','teacher', 'abbreviation', 'actions']" type="ucs"></DataTable>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import DataTable from "@/components/DataTable.vue";
 import { mapGetters } from "vuex";
 export default {
   name: "BackofficeUcs",
+  components: { DataTable },
   data() {
     return {
       form: {
         name: "",
         teacher: ""
       },
-      Users: []
+      Ucs: []
     };
   },
   methods: {
+    addUc() {
+      const uc = this.getUcByName(this.form.name);
+      if (uc) {
+        this.$swal({
+          text: "Já existe uc com esse nome!",
+          icon: "error",
+          timer: 2000,
+          showConfirmButton: false
+        });
+      } else {
+        const newUc = {
+          id: this.getUcNextId,
+          name: this.form.name,
+          teacher: this.form.teacher,
+          abbreviation: this.getAbbreviationByName(this.form.name)
+        }
+        this.$store.commit("ADD_UC", newUc);
+        this.form.name = "";
+        this.form.teacher = "";
+        this.$swal({
+          text: `${newUc.name} Adicionada!`,
+          icon: "success",
+          timer: 2000,
+          showConfirmButton: false
+        });
+      }
+    },
+    getAbbreviationByName(name) {
+      let data = name.split(' ')
+      let output = "";
+
+      for (var i = 0; i < data.length; i++) {
+        output += data[i].substring(0, 1);
+      }
+      return output;
+    },
   },
   computed: {
-    ...mapGetters(["getUserByNumber", "getTeachersByUserType"])
+    ...mapGetters(["getUcByName", "getUsersByUserType", "getUcs", "getUcNextId", "getUserById"])
   }
 };
 </script>
